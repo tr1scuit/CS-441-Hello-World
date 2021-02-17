8b13a77c780ff8c7045c17818afac02d9705a80a
Tristan Awayan <jawayan1@binghamton.edu>
2021-02-17 10:46:52 -0500

    OnTouchListener implemented
    OnClick as shown in Monday's demonstration only allows a single change when the button is clicked the first time.
    
    I looked into how to make the program more responsive and implemented OnTouchListener which allows for "held down"/"release" actions. Holding the button down changes the text/button color to green and makes the text larger, while releasing returns the elements back to normal.
    
    Also commented the code a bit

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     TextView helloText;
     Button helloButton;
@@ -16,20 +17,37 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // default init
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Objects for hello world
         helloText = (TextView) findViewById(R.id.Hello_World_Text);
         helloButton = (Button) findViewById(R.id.Hello_World_Button);
+        helloText.setTextColor(0xFF000000); helloText.setTextSize(16);
+        helloButton.setBackgroundColor(0xFF000000);
+        //helloImage = (ImageView) findViewById(R.id.Hello_World_Image);
+
+        // Interactive elements:
+        // This Button handling is programmed independent of the xml to allow for more interactivity
+        helloButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event){
+                if(event.getAction()==MotionEvent.ACTION_DOWN){
+                    helloText.setTextColor(0xFF007700);
+                    helloText.setTextSize(20);
+                    helloButton.setBackgroundColor(0xFF226600);
+                }
+                else {
+                    helloText.setTextColor(0xFF000000);
+                    helloText.setTextSize(16);
+                    helloButton.setBackgroundColor(0xFF000000);
+                }
+                return true;
+            }
+        });
 
     }
-
-    public void onHelloClick(View view){
-
-        helloText.setTextColor(0xFF00FF00);
-        helloButton.setBackgroundColor(0xFF00FF00);
-    }
-    
 
 
 }
