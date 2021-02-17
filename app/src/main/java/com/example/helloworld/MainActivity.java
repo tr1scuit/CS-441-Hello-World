30298b80c9f5e8c0d59578e609985f7f850d0785
Tristan Awayan <jawayan1@binghamton.edu>
2021-02-17 10:16:19 -0500

    OnClick events added
    According to the Monday tutorial, the button and Hello World text now changes color when you click the button

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,15 +3,33 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView helloText;
+    Button helloButton;
+    ImageView helloPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        helloText = (TextView) findViewById(R.id.Hello_World_Text);
+        helloButton = (Button) findViewById(R.id.Hello_World_Button);
+
     }
+
+    public void onHelloClick(View view){
+
+        helloText.setTextColor(0xFF00FF00);
+        helloButton.setBackgroundColor(0xFF00FF00);
+    }
+    
 
 
 }
